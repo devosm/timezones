b56292319f4b40a5ddb551791ea4edae9aa1aebf
Manu Devos <manudevos@gmail.com>
2021-08-17 20:57:50 +0200

    Switched to vars

--- index.js
+++ index.js
@@ -1,54 +1,64 @@
-let currentTime = new Date()
+var currentTime = new Date()
 let weekOfDayEl = document.getElementById("weekOfDay-el")
 let refresh = 1000
 let daysArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let monthsArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
 //Time variables
-let year = currentTime.getFullYear()
-let month = currentTime.getMonth() + 1
-let day = currentTime.getDate()
-let hour = currentTime.getHours()
-let minute = currentTime.getMinutes()
-let second = currentTime.getSeconds()
-let dayOfWeek = currentTime.getDay()
-let dayOfMonth = 0
+var year = currentTime.getFullYear()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var hour = currentTime.getHours()
+var minute = currentTime.getMinutes()
+var second = currentTime.getSeconds()
+var dayOfWeek = currentTime.getDay()
+var dayOfMonth = 0
+
+
+//Update variables
+function updateTime(){
+    currentTime = new Date()
+    year = currentTime.getFullYear()
+    month = currentTime.getMonth() + 1
+    day = currentTime.getDate()
+    hour = currentTime.getHours()
+    minute = currentTime.getMinutes()
+    second = currentTime.getSeconds()
+    dayOfWeek = currentTime.getDay()
+    dayOfMonth = 0
+}
 
 //For testing purposes
-console.log(day + "/" + month + "/" + year)
+console.log("Date: " + day + "/" + month + "/" + year)
 
 //Day of week
 
-console.log(daysArray[dayOfWeek])
+console.log("Day of week: " + daysArray[dayOfWeek])
 
 //Day of Month
 
-if (day === 1){
-    dayOfMonth = day + "st"
-} else if (day === 2){
-    dayOfMonth = day + "nd"
-} else if (day === 3){
-    dayOfMonth = day + "rd"
-} else if (day < 3 && day > 21){
-    dayOfMonth = day + "th"
-} else if (day === 21){
-    dayOfMonth = day + "st"
-} else if (day === 22){
-    dayOfMonth = day + "nd"
-} else if (day < 22 && day > 31){
-    dayOfMonth = day + "th"
-} else  if (day === 31){
-    dayOfMonth = day + "st"
+function displayWeekday(){
+    if(day === 1 || day === 21 || day == 31){
+        dayOfMonth = day + "st"
+    } else if(day === 2){
+        dayOfMonth = day + "nd"
+    } else if(day === 3){
+        dayOfMonth = day + "rd"
+    } else{
+        dayOfMonth = day + "th"
+    }
+    console.log(dayOfMonth + " of " + monthsArray[month - 1])
 }
 
-
-//Time
-console.log(hour + ":" + minute + ":" + second)
+displayWeekday()
 
 function displayTime(){
-    let currentTime = new Date()
-    weekOfDayEl.textContent = "Today is " + daysArray[dayOfWeek]
     console.log(currentTime)
+    console.log("Date: " + day + "/" + month + "/" + year)
+    console.log(hour + ":" + minute + ":" + second)
 }
 
-displayTime()
+setInterval(updateTime, refresh)
 setInterval(displayTime, refresh)
+
+//Time

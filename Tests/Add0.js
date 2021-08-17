83280d628959ce0d21e3686ed0fb9dee8cecc60d
Manu Devos <manudevos@gmail.com>
2021-08-17 21:45:23 +0200

    Solved: Adds 0 in front of hour, minute, second if below 10

--- Tests/Add0.js
+++ Tests/Add0.js
@@ -21,7 +21,20 @@ function updateTime(){
     month = currentTime.getMonth() + 1
     day = currentTime.getDate()
     hour = currentTime.getHours()
+    if (hour.toString().length < 2){
+        hour = "0" + hour
+    }
     minute = currentTime.getMinutes()
+    if (minute.toString().length < 2){
+        minute = "0" + minute
+    }
     second = currentTime.getSeconds()
+    if (second.toString().length < 2){
+        second = "0" + second
+    }
     dayOfWeek = currentTime.getDay()
+    console.log(hour + ":" + minute + ":" + second)
 }
+
+updateTime()
+setInterval(updateTime, 1000)

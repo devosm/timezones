7e684dd8f3cf6214a2e46ede614fb7cbeaf1c2a4
Manu Devos <manudevos@gmail.com>
2021-08-18 10:45:42 +0200

    Split time and day updates - to be tested

---
+++ Tests/SplitUpdate.js
@@ -0,0 +1,72 @@
+var currentTime = new Date()
+let weekOfDayEl = document.getElementById("weekOfDay-el")
+let daysArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+let monthsArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+//Time variables
+var year = currentTime.getFullYear()
+var month = currentTime.getMonth() + 1
+var day = currentTime.getDate()
+var hour = currentTime.getHours()
+var minute = currentTime.getMinutes()
+var second = currentTime.getSeconds()
+var dayOfWeek = currentTime.getDay()
+var dayOfMonth = 0
+
+//Update variables
+function updateTime(){
+    currentTime = new Date()
+    hour = currentTime.getHours()
+    if (hour.toString().length < 2){
+        hour = "0" + hour
+    }
+    minute = currentTime.getMinutes()
+    if (minute.toString().length < 2){
+        minute = "0" + minute
+    }
+    second = currentTime.getSeconds()
+    if (second.toString().length < 2){
+        second = "0" + second
+    }
+    console.log(hour + ":" + minute + ":" + second)
+}
+
+function updateDay(){
+    currentTime = new Date()
+    year = currentTime.getFullYear()
+    month = currentTime.getMonth() + 1
+    day = currentTime.getDate()
+    dayOfWeek = currentTime.getDay()    
+}
+//Function to display day of month
+
+function displayDayOfMonth(){
+    if(day === 1 || day === 21 || day == 31){
+        dayOfMonth = day + "st"
+    } else if(day === 2 || day === 22) {
+        dayOfMonth = day + "nd"
+    } else if(day === 3 || day === 23){
+        dayOfMonth = day + "rd"
+    } else{
+        dayOfMonth = day + "th"
+    }
+    console.log(dayOfMonth + " of " + monthsArray[month - 1])
+}
+
+displayDayOfMonth()
+
+//Output of time
+
+function displayTime(){
+    displayDayOfMonth()
+    console.log("Date: " + day + "/" + month + "/" + year)
+    console.log("Time: " + hour + ":" + minute + ":" + second)
+    console.log("Day of week: " + daysArray[dayOfWeek])
+}
+
+setInterval(updateTime, 1000)
+setInterval(displayTime, 1000)
+
+if(hour === 0 && minute === 0){
+    updateDay()
+}
